a61f30ddc0d1a42603c0f480f7864cbc7e04b7b2
khainn <ngockhaibk@gmail.com>
2022-12-07 17:58:00 +0700

    update login

--- YummyShop/views.py
+++ YummyShop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from store.models import Product, Event
 from category.models import Category
+from django.core.paginator import Paginator
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
     categoris = Category.objects.all()
     events = Event.objects.all()
+
+    # paginator = Paginator(products, 3)
+    # page_number = request.GET.get('page')
+    # page_products = paginator.get_page(page_number)
+
     context = {
         'products': products,
         'categoris' : categoris,
