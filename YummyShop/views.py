5a1c27ef678187dabcb37086ae8f8a072960f884
khainn <ngockhaibk@gmail.com>
2022-12-13 17:52:45 +0700

    update add to cart

--- YummyShop/views.py
+++ YummyShop/views.py
@@ -2,19 +2,58 @@ from django.shortcuts import render
 from store.models import Product, Event
 from category.models import Category
 from django.core.paginator import Paginator
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+from orders.models import OrderProduct
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
     categoris = Category.objects.all()
     events = Event.objects.all()
 
-    # paginator = Paginator(products, 3)
-    # page_number = request.GET.get('page')
-    # page_products = paginator.get_page(page_number)
+    try:   
+        cart = Cart.objects.get(cart_id=_cart_id(request=request))
+        in_cart = CartItem.objects.filter(
+            cart=cart,
+        ).exists()
+    except Exception as e:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+
 
     context = {
+        'in_cart': in_cart if 'in_cart' in locals() else False,
         'products': products,
         'categoris' : categoris,
         'events': events
     }
     return render(request, 'home.html', context=context)
+
+def add_cart(request):
+    if(request.method == "POST"):
+        current_user = request.user
+        product = Product.objects.get(id=16)    # Get object product
+        if current_user.is_authenticated:
+            for item in request.POST:
+                key = item
+                value = request.POST.get(key)
+                print("key: ", key)
+                print("value: ", value)
+
+        is_exists_cart_item = CartItem.objects.filter(product=product, user=current_user).exists()
+        
+        if is_exists_cart_item:           
+            cart_item = CartItem.objects.get(
+                product=product,
+                user=current_user
+            )
+            cart_item.quantity += 1
+        else:
+            cart_item = CartItem.objects.create(
+                    product=product,
+                    user=current_user,
+                    quantity=1
+                )
+        cart_item.save()
+    
